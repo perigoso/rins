ea8cd76b18fd3e144f1c108b9ecbf2c83243a522
Rafael Silva <perigoso@riseup.net>
2023-02-16 23:46:38 +0000

    Initial commit

---
+++ conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = "RINS"
+copyright = "2023, RINS Contributors"
+author = "RINS Contributors"
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx_autodoc_typehints",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}
+
+templates_path = ["_templates"]
+
+exclude_patterns = [".github/*", "LICENSE", "build/*"]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "furo"
+html_title = "RINS"
+html_static_path = ["_static"]
+
+todo_include_todos = True
+
+autoclass_content = "both"
---
+++ license.rst
@@ -0,0 +1,10 @@
+License
+=======
+
+This project is licensed under the Creative Commons `CC-BY-SA <https://creativecommons.org/licenses/by-sa/4.0/>`_. The full text of which is included below
+
+CC BY-SA 4.0
+------------
+
+.. include:: LICENSE
+	:literal:
